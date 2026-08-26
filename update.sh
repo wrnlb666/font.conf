855410d5493d223d1aee8cebd2b832bd49401474
wrnlb666 <wrnlb666@gmail.com>
2026-08-27 01:52:44 +1000

    download maple mono nf cn fonts family with the script

--- update.sh
+++ update.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-[[ -d ~/.config ]] || mkdir ~/.config
-[[ -e ~/.config/fontconfig/ ]] && rm -rf ~/.config/fontconfig
+# install font config
+[[ -d ~/.config ]] || mkdir -p ~/.config
+[[ -d ~/.config/fontconfig ]] || mkdir -p ~/.config/fontconfig
+ln -sf "$(pwd)/fonts.conf" ~/.config/fontconfig/fonts.conf
 
-ln -sf "$(pwd)" ~/.config/fontconfig
+# install maple mono if necessary
+if ! fc-list : family | grep -i "Maple Mono NF CN" >/dev/null; then
+    target="https://github.com/subframe7536/maple-font/releases/latest/download/MapleMono-NF-CN.zip"
+    [[ -d ~/.local/share/fonts/MapleMono-NF-CN ]] || mkdir -p ~/.local/share/fonts/MapleMono-NF-CN
+    (
+        cd ~/.local/share/fonts/MapleMono-NF-CN || exit 1
+        echo '[INFO] Downloading MapleMono-NF-CN...'
+        curl -fsSL -o MapleMono-NF-CN.zip "$target" || exit 1
+        echo '[INFO] Deflating MapleMono-NF-CN.zip...'
+        unzip -oqq MapleMono-NF-CN.zip
+    )
+fi
